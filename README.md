86bd73dd00c2000f8a27923668ccbb7b607b6ed4
SebastianJast <sebastian.jast.programista@gmail.com>
2024-09-20 21:06:40 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+View an interactive CV webpage with sections for contact details, skills, education, experience, and more.
+Download the CV as a PDF.
+Navigate through the sections using the navigation bar.
+
+### Screenshot
+
+![Screenshot solution](./img/screenshot.JPG)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://sebastianjast.github.io/Responsive-CV/)
+
+## My process
+
+### Built with
+
+- HTML5 for structuring the CV content.
+- CSS3 with Flexbox for layout and styling.
+- JavaScript for interactive elements (e.g., theme switching, generating PDF).
+- Boxicons for icons.
+- html2pdf.js for PDF generation.
+
+### What I learned
+
+During this project, I enhanced my skills in:
+
+- Responsive Design: I used Flexbox and CSS techniques to create a layout that adjusts to different screen sizes.
+- PDF Generation: I integrated html2pdf.js to allow users to download the CV as a PDF directly from the page.
+- Interactive Features: I implemented JavaScript-based functionalities like theme switching and a button to generate the PDF, which improved the usability of the site.
+
+## Author
+
+- GitHub - [@yourusername](https://github.com/SebastianJast)
---
+++ js/main.js
@@ -0,0 +1,106 @@
+/* ===== SHOW MENU ===== */
+const btnToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+const toogleNavMenu = () => {
+	navMenu.classList.toggle('show-menu');
+};
+
+btnToggle.addEventListener('click', toogleNavMenu);
+
+/* ===== REMOVE MENU MOBILE ===== */
+const navLinkBtns = document.querySelectorAll('.nav__link');
+const nav = document.getElementById('nav-menu');
+
+const linkAction = () => {
+	nav.classList.remove('show-menu');
+};
+
+navLinkBtns.forEach(nBtn => nBtn.addEventListener('click', linkAction));
+
+/* ===== SCROLL SECTIONS ACTIVE LINK ===== */
+
+/* ===== CHANGE BACKGROUND HEADER ===== */
+
+/* ===== SHOW SCROLL TOP ===== */
+const scrollTop = () => {
+	const scrollTop = document.getElementById('scroll-top');
+
+	if (this.scrollY >= 200) {
+		scrollTop.classList.add('show-scroll');
+	} else {
+		scrollTop.classList.remove('show-scroll');
+	}
+};
+
+window.addEventListener('scroll', scrollTop);
+
+/* ===== DARK LIGHT THEME ===== */
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => {
+	document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+};
+
+const getCurrentIcon = () => {
+	themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+};
+
+if (selectedTheme) {
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+	themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+	document.body.classList.toggle(darkTheme);
+	themeButton.classList.toggle(iconTheme);
+
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+/* ===== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ===== */
+
+function scaleCv() {
+	document.body.classList.add('scale-cv');
+}
+
+/* ===== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ===== */
+
+function removeScale() {
+	document.body.classList.remove('scale-cv');
+	footer.classList.remove('active');
+}
+
+/* ===== GENERATE PDF ===== */
+
+let areaCv = document.getElementById('area-cv');
+
+let resumeButton = document.getElementById('resume-button');
+
+const footer = document.querySelector('footer');
+
+let opt = {
+	margin: [2, 0, 1, 2],
+	filename: 'Sebastian_Jastrzebski_CV',
+	image: { type: 'jpeg', quality: 0.98 },
+	html2canvas: { scale: 5 },
+	jsPDF: { unit: 'mm', format: [210, 294], orientation: 'portrait' },
+};
+
+function generateResume() {
+	// html2pdf(areaCv, opt);
+	html2pdf().from(areaCv).set(opt).save();
+}
+resumeButton.addEventListener('click', () => {
+	footer.classList.add('active');
+	scaleCv();
+	generateResume();
+	setTimeout(removeScale, 1000);
+});
